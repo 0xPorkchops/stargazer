e37e25f8d9e3dc2a5a5a25efb3f4d6954c99d731
0xPorkchops <proulxcameron@icloud.com>
2024-10-25 11:02:37 -0400

    Initialized Clerk in React and Express. Fixed indentation in scripts.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,20 @@
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import {
+  ClerkExpressRequireAuth,
+  ClerkExpressWithAuth,
+} from "@clerk/clerk-sdk-node";
+import "dotenv/config"; // To read CLERK_SECRET_KEY and CLERK_PUBLISHABLE_KEY
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
@@ -18,17 +25,19 @@ mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch(err => console.error('MongoDB connection error:', err));
 */
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
+app.use(express.static(path.join(__dirname, "..", "..", "client", "build")));
 
 // API routes
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello from Express!' });
+app.get("/api/hello", ClerkExpressRequireAuth(), (req, res) => {
+  res.send({ message: "Hello from Express! You are logged in :)" });
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "..", "client", "build", "index.html")
+  );
 });
 
 app.listen(port, () => {
