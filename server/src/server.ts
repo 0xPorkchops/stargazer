43d85976381a587f6eab83eb5e0a3b85179a9b1d
0xPorkchops <proulxcameron@icloud.com>
2024-11-03 21:32:01 -0500

    Testing express routing on vercel

--- server/src/server.ts
+++ server/src/server.ts
@@ -82,9 +82,9 @@ async function startServer() {
 
     // The "catchall" handler: for any request that doesn't
     // match one above, send back React's index.html file.
-    app.get('*', (req, res) => {
+    /*app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html'));
-    });
+    });*/
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
