9d48e5eda04bcba61093b8a62b4085acc3882b9f
Stanley9986 <yangstanley2022@gmail.com>
2024-11-16 11:58:58 -0500

    Universal Nav-Bar, StarMap API and UI, HomePage

---
+++ client/src/Layout.tsx
@@ -0,0 +1,46 @@
+import { Button } from './components/ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton} from '@clerk/clerk-react'
+
+function Layout({ children }: { children: React.ReactNode }) {
+
+  return (
+    <>
+        <header className="sticky top-0 z-10 bg-gray-800 text-white">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex flex-col sm:flex-row justify-between items-center py-4">
+            {/* Logo */}
+            <div className="flex items-center mb-4 sm:mb-0">
+                <img src="/SGlogo2.png" alt="Star Gazers Logo" className="h-16 sm:h-24 mr-4" />
+            </div>
+
+            {/* Navigation Tabs */}
+            <nav className="flex flex-wrap justify-center sm:justify-end space-x-3 sm:space-x-5">
+                <a href="/starmap" className="px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">Weather & Star Map</a>
+                <a href="/locationtest" className="px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">Location Test </a>
+                <a href="/" className="px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">Home</a>
+            </nav>
+
+            {/* User Actions */}
+            <div className="mt-4 sm:mt-0">
+            <SignedOut>
+            <SignInButton>
+                <Button variant="secondary">
+                Sign Up
+                </Button>
+                </SignInButton>
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+            </div>
+            </div>
+        </div>
+        </header>
+        <main>
+            {children}
+        </main>
+    </>
+  )
+}
+
+export default Layout
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,17 +1,13 @@
-import SignOut from '../components/SignOut'
-import { useNavigate } from 'react-router-dom';
-import { Button } from '../components/ui/Button'
-function HomePage(){
-    const navigate = useNavigate();
-    return (
-    <>
-        <h1> Welcome to HomePage. </h1>
-        <SignOut />
-        <Button onClick={()=>{navigate('/weather')}}>Weather Map</Button>
-        <Button onClick={()=>{navigate('/locationtest')}}>Testing</Button>
-        
-    </>
-    )
+function HomePage() {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
+      <div className = "flex flex-col items-center justify-center min-h-screen bg-gray-900">
+        <img src="/SGlogo2.png" alt="Star Gazers Logo" className="h-80 sm:h-112" />
+        <p className="text-white text-[3rem] mt-4">Welcome to StarGazer!</p>
+      </div>
+    </div>
+  )
 }
 
-export default HomePage;
+export default HomePage
