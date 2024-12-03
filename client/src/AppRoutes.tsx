a4cb11113a9f3a05228cef0d3c610223089d8b8b
Benson Zheng <bensonzheng2003@gmail.com>
2024-12-02 20:22:25 -0500

    removed location test page

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -2,8 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/HomePage';
 import Weather from './pages/WeatherPage';
 import StarMap from './pages/StarMap';
-import TestingPage from './pages/TestingPage';
-import LocationTest from './test/LocationTest';
 
 function AppRoutes() {
   return (
@@ -11,8 +9,6 @@ function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/weather" element={<Weather />} />
       <Route path="/starmap" element={<StarMap />} />
-      <Route path="/testing" element={<TestingPage />} />
-      <Route path="/locationtest" element={<LocationTest />} />
     </Routes>
   );
 }
