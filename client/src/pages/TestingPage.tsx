5949291478d60c1ed3953e56157a7330e92fc16c
0xPorkchops <proulxcameron@icloud.com>
2024-12-08 19:27:47 -0500

    Deleted Settings component from testing page

--- client/src/pages/TestingPage.tsx
+++ client/src/pages/TestingPage.tsx
@@ -1,10 +1,8 @@
-import { Settings } from '@/components/Settings';
 import { Button } from '../components/ui/button';
 import { useNavigate } from 'react-router-dom';
 export default function TestingPage(){
     const navigate = useNavigate();
     return (<>
-        {/* <Settings /> */}
         <Button onClick={()=>{navigate('/testing')}}>Location Test</Button>
     </>)
 }
